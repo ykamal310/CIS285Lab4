cf8613d95d43618d49de0aec13e2d7193b1e0464
ykamal310 <ykamal@umich.edu>
2025-10-02 14:11:01 -0400

    Update Hello.java
    adds goodbye world

--- Hello.java
+++ Hello.java
@@ -3,5 +3,9 @@ public class Hello {
         for(int i=1;i<=10;i++) {
             System.out.println("Hello World"+i);
         }
+
+        for(int j=1;j<=10;j++) {
+            System.out.println("Goodbye World"+j);
+        }
     }
 }
